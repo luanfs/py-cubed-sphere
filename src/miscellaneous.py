e955ed7a61c965f0439cc39f08af74997f7909df
luanfs <luanfonseca93@gmail.com>
2022-08-11 16:28:37 -0300

    ident minor changes

--- src/miscellaneous.py
+++ src/miscellaneous.py
@@ -15,31 +15,31 @@ from constants import griddir, datadir, graphdir
 # Reference: https://gist.github.com/keithweaver/562d3caa8650eefe7f84fa074e9ca949
 ####################################################################################
 def createFolder(dir):
-   try:
-      if not os.path.exists(dir):
-         os.makedirs(dir)
-   except OSError:
-      print ('Error: Creating directory. '+ dir)
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        print ('Error: Creating directory. '+ dir)
 
 ####################################################################################
 # Create the needed directories
 ####################################################################################       
 def createDirs():
-   print("--------------------------------------------------------")
-   print("Cubed-sphere grid generator by Luan Santos - 2022\n")
-    # Check directory grid does not exist
-   if not os.path.exists(griddir):
-      print('Creating directory ',griddir)
-      createFolder(griddir)
+    print("--------------------------------------------------------")
+    print("Cubed-sphere grid generator by Luan Santos - 2022\n")
+        # Check directory grid does not exist
+    if not os.path.exists(griddir):
+        print('Creating directory ',griddir)
+        createFolder(griddir)
 
-   # Check directory data does not exist   
-   if not os.path.exists(datadir):
-      print('Creating directory ',datadir)
-      createFolder(datadir) 
+    # Check directory data does not exist   
+    if not os.path.exists(datadir):
+        print('Creating directory ',datadir)
+        createFolder(datadir) 
 
-   # Check directory graphs does not exist
-   if not os.path.exists(graphdir):
-      print('Creating directory ',graphdir)
-      createFolder(graphdir)
+    # Check directory graphs does not exist
+    if not os.path.exists(graphdir):
+        print('Creating directory ',graphdir)
+        createFolder(graphdir)
 
-   print("--------------------------------------------------------")      
+    print("--------------------------------------------------------")      
