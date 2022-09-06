135eeaaa125e31e5d92840749d36a8acc290c210
luanfs <luanfonseca93@gmail.com>
2022-09-06 13:46:41 -0300

    added advection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##########################################################
+#########################################################
 #
 # Cubed-sphere generation code
 #
@@ -14,56 +14,68 @@ import os.path
 sys.path.append(srcdir)
 
 # Imports
-from miscellaneous import createDirs
-from configuration import get_parameters
-from cs_datastruct import cubed_sphere, latlon_grid
-from interpolation import ll2cs
-from gridquality   import grid_quality
-from plot          import plot_grid, save_grid_netcdf4
-from constants     import Nlat, Nlon
-#from divergence    import divergence_test
+from miscellaneous      import createDirs
+from configuration      import get_parameters, get_advection_parameters
+from cs_datastruct      import cubed_sphere, latlon_grid
+from interpolation      import ll2cs
+from gridquality        import grid_quality
+from plot               import plot_grid, save_grid_netcdf4
+from constants          import Nlat, Nlon
+from advection_ic       import adv_simulation_par_2d
+from advection_sphere   import adv_sphere
 
 def main():
-   # Create the directories
-   createDirs()
+    # Create the directories
+    createDirs()
 
-   # Get the parameters
-   N, transformation, showonscreen, gridload, test_case, map_projection = get_parameters()
+    # Get the parameters
+    N, transformation, showonscreen, gridload, test_case, map_projection = get_parameters()
 
-   # Select test case
-   if test_case == 1:    
-      print("Test case 1: cubed-sphere generation and plotting.\n")
-      # Create CS mesh
-      cs_grid = cubed_sphere(N, transformation, showonscreen, gridload)
+    # Select test case
+    if test_case == 1:    
+        print("Test case 1: cubed-sphere generation and plotting.\n")
+        # Create CS mesh
+        cs_grid = cubed_sphere(N, transformation, showonscreen, gridload)
 
-      # Mesh plot      
-      plot_grid(cs_grid, map_projection)
+        # Mesh plot      
+        plot_grid(cs_grid, map_projection)
       
-      # Save grid in netcdf format
-      if not(os.path.isfile(cs_grid.netcdfdata_filename)) or (os.path.isfile(cs_grid.netcdfdata_filename) and gridload==False):
-         save_grid_netcdf4(cs_grid)
+        # Save grid in netcdf format
+        if not(os.path.isfile(cs_grid.netcdfdata_filename)) or (os.path.isfile(cs_grid.netcdfdata_filename) and gridload==False):
+            save_grid_netcdf4(cs_grid)
 
-   else:
-      # Create the CS mesh
-      cs_grid = cubed_sphere(N, transformation, showonscreen, gridload)
+    else:
+        # Create the CS mesh
+        cs_grid = cubed_sphere(N, transformation, showonscreen, gridload)
 
-      # Save grid in netcdf format
-      if not(os.path.isfile(cs_grid.netcdfdata_filename)) or (os.path.isfile(cs_grid.netcdfdata_filename) and gridload==False):
-         save_grid_netcdf4(cs_grid)
+        # Save grid in netcdf format
+        if not(os.path.isfile(cs_grid.netcdfdata_filename)) or (os.path.isfile(cs_grid.netcdfdata_filename) and gridload==False):
+            save_grid_netcdf4(cs_grid)
 
-      # Create the latlon mesh (for plotting)  
-      ll_grid = latlon_grid(Nlat, Nlon)
-      ll_grid.ix, ll_grid.jy, ll_grid.mask = ll2cs(cs_grid, ll_grid)
-      if test_case == 2:
-         print("Test case 2: grid quality test.\n")
-         # Call grid quality test
-         grid_quality(cs_grid, ll_grid, map_projection)
+        # Create the latlon mesh (for plotting)  
+        ll_grid = latlon_grid(Nlat, Nlon)
+        ll_grid.ix, ll_grid.jy, ll_grid.mask = ll2cs(cs_grid, ll_grid)
+        if test_case == 2:
+            print("Test case 2: grid quality test.\n")
+            # Call grid quality test
+            grid_quality(cs_grid, ll_grid, map_projection)
 
-      #elif test_case == 3:
-      #   # Call divergence test
-      #   print("Test case 3: Divergence test.\n")
-      #   divergence_test(cs_grid, ll_grid, map_projection)
-      else:
-         print("ERROR: invalid testcase.")
-         exit()
+        elif test_case == 3:
+            # Call advection test case
+            print("Test case 3: Advection test case.\n")
+            dt, Tf, tc, ic, mono = get_advection_parameters()
+            simulation = adv_simulation_par_2d(dt, Tf, ic, tc, mono)
+
+            if simulation.tc == 1: # Advection on the sphere simalution
+                adv_sphere(cs_grid, ll_grid, simulation, map_projection)
+
+            elif simulation.tc == 2: # Convergence analysis
+                print('Not implemented yet =(')
+
+            else:
+                print('Invalid advection testcase.\n')
+                exit()
+        else:
+            print("ERROR: invalid testcase.")
+            exit()
 main()
--- src/configuration.py
+++ src/configuration.py
@@ -11,7 +11,6 @@ from constants import*
 import os.path
 
 def get_parameters():
-
    # The standard grid file configuration.par must exist in par/ directory 
    file_path = pardir+"configuration.par"
    
@@ -82,3 +81,54 @@ def get_parameters():
       exit()
 
    return N, transf, showonscreen, gridload, test_case, map
+   
+   
+####################################################################################
+# Get parameters for advection test case
+####################################################################################
+def get_advection_parameters():
+    # The standard grid file configuration.par must exist in par/ directory 
+    file_path = pardir+"advection.par"
+
+    if os.path.exists(file_path): # The file exists
+        # Open the grid file
+        confpar = open(file_path, "r")
+
+        # Read the grid file lines
+        confpar.readline()
+        confpar.readline()
+        Tf = confpar.readline()
+        confpar.readline()
+        dt = confpar.readline()
+        confpar.readline()
+        ic = confpar.readline()
+        confpar.readline()
+        tc = confpar.readline()
+        confpar.readline()
+        mono = confpar.readline()
+        confpar.readline()
+
+        # Close the file
+        confpar.close()
+
+        # Convert from str to int
+        Tf = float(Tf)
+        dt = float(dt)
+        ic = int(ic)
+        tc = int(tc)
+        mono = int(mono)
+
+        #Print the parameters on the screen
+        print("\n--------------------------------------------------------")
+        print("Parameters from file", file_path,"\n")
+        print("Total time of integration: ", Tf)
+        print("Time step: ", dt)
+        print("Initial condition: ", ic)
+        print("Adv test case: ", tc)
+        print("Monotonization scheme: ", mono)
+        print("--------------------------------------------------------\n")
+
+    else:   # The file does not exist
+        print("ERROR in get_grid_parameters: file "+ filename +" not found in /par.")
+        exit()
+    return dt, Tf, tc, ic, mono
