1ff5a8c6040d4c61688372245a95cf3708d04b05
luanfs <luanfonseca93@gmail.com>
2022-09-30 14:47:36 -0300

    minor changes

--- src/cs_datastruct.py
+++ src/cs_datastruct.py
@@ -1,9 +1,9 @@
 ####################################################################################
-# 
+#
 # Module for cubed-sphere mesh generation and structuring.
 #
 # Based on "C. Ronchi, R. Iacono, P.S. Paolucci, The “Cubed Sphere”:
-# A New Method for the Solution of Partial Differential Equations 
+# A New Method for the Solution of Partial Differential Equations
 # in Spherical Geometry."
 #
 # Luan da Fonseca Santos - January 2022
@@ -20,7 +20,7 @@ import netCDF4 as nc
 
 ####################################################################################
 #  Global cubed-sphere grid structure
-####################################################################################      
+####################################################################################
 class cubed_sphere:
     def __init__(self, N, transformation, showonscreen, gridload):
         # Panel indexes distribution
@@ -42,19 +42,19 @@ class cubed_sphere:
         # Grid name
         self.projection = transformation
         self.name = self.projection+"_"+str(N)
-      
+
         # Grid data netcdf filename
         self.netcdfdata_filename = griddir+self.name+".nc"
 
         # Load the grid
         if gridload==True and (os.path.isfile(self.netcdfdata_filename)):   # Check if grid file exists
             if showonscreen==True:
-                print("--------------------------------------------------------")      
+                print("--------------------------------------------------------")
                 print('Loading grid file '+self.netcdfdata_filename)
-         
+
             # Start time counting
             start_time = time.time()
-          
+
             # Open grid data
             griddata = nc.Dataset(self.netcdfdata_filename,'r')
 
@@ -67,7 +67,7 @@ class cubed_sphere:
             self.nghost        = griddata['nghost'][:]
             self.nghost_left   = griddata['nghost_left'][:]
             self.nghost_right  = griddata['nghost_right'][:]
- 
+
             # Interior indexes
             self.i0, self.iend = griddata['i0'][:], griddata['iend'][:]
             self.j0, self.jend = griddata['j0'][:], griddata['jend'][:]
@@ -80,13 +80,13 @@ class cubed_sphere:
             centers  = point(N+nghost, N+nghost)
             edx      = point(N+1+nghost, N+nghost)
             edy      = point(N+nghost, N+1+nghost)
-            
+
             # Tangent vectors
             tg_ex_edx = point(N+1+nghost, N+nghost)
             tg_ey_edx = point(N+1+nghost, N+nghost)
             tg_ex_edy = point(N+nghost  , N+1+nghost)
             tg_ey_edy = point(N+nghost  , N+1+nghost)
-            
+
             elon_edx = np.zeros((N+1+nghost, N+1+nghost, nbfaces, 3))
             elat_edx = np.zeros((N+1+nghost, N+nghost, nbfaces, 3))
             elon_edy = np.zeros((N+nghost, N+1+nghost, nbfaces, 3))
@@ -97,13 +97,13 @@ class cubed_sphere:
             vertices.Y   = griddata['vertices'][:,:,:,1]
             vertices.Z   = griddata['vertices'][:,:,:,2]
             vertices.lon = griddata['vertices'][:,:,:,3]
-            vertices.lat = griddata['vertices'][:,:,:,4]         
-         
+            vertices.lat = griddata['vertices'][:,:,:,4]
+
             centers.X   = griddata['centers'][:,:,:,0]
             centers.Y   = griddata['centers'][:,:,:,1]
             centers.Z   = griddata['centers'][:,:,:,2]
             centers.lon = griddata['centers'][:,:,:,3]
-            centers.lat = griddata['centers'][:,:,:,4]  
+            centers.lat = griddata['centers'][:,:,:,4]
 
             edx.X   = griddata['edx'][:,:,:,0]
             edx.Y   = griddata['edx'][:,:,:,1]
@@ -122,7 +122,7 @@ class cubed_sphere:
             tg_ex_edx.Y   = griddata['tg_ex_edx'][:,:,:,1]
             tg_ex_edx.Z   = griddata['tg_ex_edx'][:,:,:,2]
             tg_ex_edx.lon = griddata['tg_ex_edx'][:,:,:,3]
-            tg_ex_edx.lat = griddata['tg_ex_edx'][:,:,:,4]            
+            tg_ex_edx.lat = griddata['tg_ex_edx'][:,:,:,4]
 
             tg_ey_edx.X   = griddata['tg_ey_edx'][:,:,:,0]
             tg_ey_edx.Y   = griddata['tg_ey_edx'][:,:,:,1]
@@ -196,7 +196,7 @@ class cubed_sphere:
             self.areas           = areas
 
             self.metric_tensor_centers = metric_tensor_centers
-            self.metric_tensor_edy     = metric_tensor_edx
+            self.metric_tensor_edx     = metric_tensor_edx
             self.metric_tensor_edy     = metric_tensor_edy
 
             self.prod_ex_elon_edx = prod_ex_elon_edx
@@ -213,16 +213,16 @@ class cubed_sphere:
 
             # Close netcdf file
             griddata.close()
-         
+
             # Finish time counting
             elapsed_time = time.time() - start_time
         else:
             # Generate the grid
             if showonscreen==True:
-                print("--------------------------------------------------------")      
+                print("--------------------------------------------------------")
                 print("Generating "+self.projection+" cubed-sphere with "+str(nbfaces*N*N)+" cells...")
 
-            if transformation == "gnomonic_equiangular":  
+            if transformation == "gnomonic_equiangular":
                 x_min, x_max, y_min, y_max = [-pio4, pio4, -pio4, pio4] # Angular coordinates
             elif transformation == "gnomonic_equidistant":
                 a = self.R/np.sqrt(3.0)  # Half length of the cube
@@ -233,7 +233,7 @@ class cubed_sphere:
             else:
                 print("ERROR: invalid grid transformation.")
                 exit()
-      
+
             # Start time counting
             start_time = time.time()
 
@@ -244,7 +244,7 @@ class cubed_sphere:
 
             # Ghost cells for each panel
             if transformation == "gnomonic_equiangular" or transformation == "gnomonic_equidistant":
-                nghost_left  = 2
+                nghost_left  = 3
                 nghost_right = 3
             #elif transformation == "conformal":
             #    nghost_left  = 0
@@ -252,7 +252,7 @@ class cubed_sphere:
 
             nghost = nghost_right + nghost_left
             self.nghost_left  = nghost_left
-            self.nghost_right = nghost_right 
+            self.nghost_right = nghost_right
             self.nghost = nghost
 
             # Interior indexes
@@ -269,7 +269,7 @@ class cubed_sphere:
 
             vertices = point(N+1+nghost, N+1+nghost)
 
-            if transformation == "gnomonic_equiangular": 
+            if transformation == "gnomonic_equiangular":
                 vertices.X, vertices.Y, vertices.Z, vertices.lon, vertices.lat = equiangular_gnomonic_map(x, y, N+1+nghost, N+1+nghost, self.R)
 
             elif transformation=="gnomonic_equidistant":
@@ -281,7 +281,7 @@ class cubed_sphere:
 
             # Generate cell centers
             if showonscreen==True:
-                print("Generating cell centers...")  
+                print("Generating cell centers...")
 
             centers = point(N+nghost, N+nghost)
 
@@ -299,13 +299,13 @@ class cubed_sphere:
 
             # Metric tensor on centers
             metric_tensor_centers = np.zeros((N+nghost, N+nghost, nbfaces))
-            metric_tensor_centers[:,:,0] = metric_tensor(x, y, self.R, transformation)  
-            for p in range(1, nbfaces): metric_tensor_centers[:,:,p] = metric_tensor_centers[:,:,0]  
+            metric_tensor_centers[:,:,0] = metric_tensor(x, y, self.R, transformation)
+            for p in range(1, nbfaces): metric_tensor_centers[:,:,p] = metric_tensor_centers[:,:,0]
             self.metric_tensor_centers = metric_tensor_centers
 
             # Generate cell edges in x direction
             if showonscreen==True:
-                print("Generating cell edges and tangent vectors in x direction...")  
+                print("Generating cell edges and tangent vectors in x direction...")
 
             edx       = point(N+1+nghost, N+nghost)
             tg_ex_edx = point(N+1+nghost, N+nghost)
@@ -336,16 +336,16 @@ class cubed_sphere:
             # Normalize the tangent vectors in x dir
             norm = tg_ex_edx.X*tg_ex_edx.X + tg_ex_edx.Y*tg_ex_edx.Y + tg_ex_edx.Z*tg_ex_edx.Z
             norm = np.sqrt(norm)
-            tg_ex_edx.X = tg_ex_edx.X/norm
-            tg_ex_edx.Y = tg_ex_edx.Y/norm
-            tg_ex_edx.Z = tg_ex_edx.Z/norm
+            tg_ex_edx.X = tg_ex_edx.X#/norm
+            tg_ex_edx.Y = tg_ex_edx.Y#/norm
+            tg_ex_edx.Z = tg_ex_edx.Z#/norm
 
             # Normalize the tangent vectors in y dir
             norm = tg_ey_edx.X*tg_ey_edx.X + tg_ey_edx.Y*tg_ey_edx.Y + tg_ey_edx.Z*tg_ey_edx.Z
             norm = np.sqrt(norm)
-            tg_ey_edx.X = tg_ey_edx.X/norm
-            tg_ey_edx.Y = tg_ey_edx.Y/norm
-            tg_ey_edx.Z = tg_ey_edx.Z/norm
+            tg_ey_edx.X = tg_ey_edx.X#/norm
+            tg_ey_edx.Y = tg_ey_edx.Y#/norm
+            tg_ey_edx.Z = tg_ey_edx.Z#/norm
 
             #print(np.amax(abs(tg_ex_edx2.X-tg_ex_edx.X)))
             #print(np.amax(abs(tg_ex_edx2.Y-tg_ex_edx.Y)))
@@ -356,13 +356,13 @@ class cubed_sphere:
 
             # Metric tensor on edges in x direction
             metric_tensor_edx = np.zeros((N+1+nghost, N+nghost, nbfaces))
-            metric_tensor_edx[:,:,0] = metric_tensor(x, y, self.R, transformation)  
-            for p in range(1, nbfaces): metric_tensor_edx[:,:,p] = metric_tensor_edx[:,:,0]  
+            metric_tensor_edx[:,:,0] = metric_tensor(x, y, self.R, transformation)
+            for p in range(1, nbfaces): metric_tensor_edx[:,:,p] = metric_tensor_edx[:,:,0]
             self.metric_tensor_edx = metric_tensor_edx
 
             # Generate cell edges in y direction
             if showonscreen==True:
-                print("Generating cell edges and tangent vectors in y direction...")  
+                print("Generating cell edges and tangent vectors in y direction...")
 
             edy = point(N+nghost, N+nghost+1)
             tg_ex_edy = point(N+nghost, N+nghost+1)
@@ -391,16 +391,16 @@ class cubed_sphere:
             # Normalize the tangent vectors in x dir
             norm = tg_ex_edy.X*tg_ex_edy.X + tg_ex_edy.Y*tg_ex_edy.Y + tg_ex_edy.Z*tg_ex_edy.Z
             norm = np.sqrt(norm)
-            tg_ex_edy.X = tg_ex_edy.X/norm
-            tg_ex_edy.Y = tg_ex_edy.Y/norm
-            tg_ex_edy.Z = tg_ex_edy.Z/norm
+            tg_ex_edy.X = tg_ex_edy.X#/norm
+            tg_ex_edy.Y = tg_ex_edy.Y#/norm
+            tg_ex_edy.Z = tg_ex_edy.Z#/norm
 
             # Normalize the tangent vectors in y dir
             norm = tg_ey_edy.X*tg_ey_edy.X + tg_ey_edy.Y*tg_ey_edy.Y + tg_ey_edy.Z*tg_ey_edy.Z
             norm = np.sqrt(norm)
-            tg_ey_edy.X = tg_ey_edy.X/norm
-            tg_ey_edy.Y = tg_ey_edy.Y/norm
-            tg_ey_edy.Z = tg_ey_edy.Z/norm
+            tg_ey_edy.X = tg_ey_edy.X#/norm
+            tg_ey_edy.Y = tg_ey_edy.Y#/norm
+            tg_ey_edy.Z = tg_ey_edy.Z#/norm
 
             #print(np.amax(abs(tg_ex_edy2.X-tg_ex_edy.X)))
             #print(np.amax(abs(tg_ex_edy2.Y-tg_ex_edy.Y)))
@@ -411,8 +411,8 @@ class cubed_sphere:
 
             # Metric tensor on edges in y direction
             metric_tensor_edy = np.zeros((N+nghost, N+nghost+1, nbfaces))
-            metric_tensor_edy[:,:,0] = metric_tensor(x, y, self.R, transformation)  
-            for p in range(1, nbfaces): metric_tensor_edy[:,:,p] = metric_tensor_edy[:,:,0]  
+            metric_tensor_edy[:,:,0] = metric_tensor(x, y, self.R, transformation)
+            for p in range(1, nbfaces): metric_tensor_edy[:,:,p] = metric_tensor_edy[:,:,0]
             self.metric_tensor_edy = metric_tensor_edy
 
             # Compute cell lenghts
@@ -428,7 +428,7 @@ class cubed_sphere:
             p1 = np.reshape(p1,(3,(N+nghost)*(N+nghost+1)*nbfaces))
             p2 = np.reshape(p2,(3,(N+nghost)*(N+nghost+1)*nbfaces))
 
-            # Compute arclen      
+            # Compute arclen
             d = sphgeo.arclen(p1, p2)
             d = np.reshape(d,(N+nghost,N+nghost+1,nbfaces))
             self.length_x = d
@@ -450,11 +450,11 @@ class cubed_sphere:
             # Given points
             p1 = [vertices.X[0:N+nghost,1:N+nghost+1,:], vertices.Y[0:N+nghost,1:N+nghost+1,:], vertices.Z[0:N+nghost,1:N+nghost+1,:]]
             p2 = [vertices.X[1:N+nghost+1,0:N+nghost,:], vertices.Y[1:N+nghost+1,0:N+nghost,:], vertices.Z[1:N+nghost+1,0:N+nghost,:]]
-      
+
             # Reshape
             p1 = np.reshape(p1,(3,(N+nghost)*(N+nghost)*nbfaces))
             p2 = np.reshape(p2,(3,(N+nghost)*(N+nghost)*nbfaces))
-      
+
             # Compute arclen
             d = sphgeo.arclen(p1,p2)
             d = np.reshape(d,(N+nghost,N+nghost,nbfaces))
@@ -505,7 +505,7 @@ class cubed_sphere:
             c = self.length_diag              # DB
             angles[:,:,:,2] = sphgeo.tri_angle(a, b, c)
             areas = areas + sphgeo.tri_area(a, b, c)
-      
+
             # Compute the angle D using the triangle ADC
             a = self.length_x[:,1:N+nghost+1,:] # DC
             b = self.length_y[0:N+nghost,:,:]   # AD
@@ -517,7 +517,7 @@ class cubed_sphere:
 
             # Compute areas
             if showonscreen==True:
-                print("Computing cell areas...")    
+                print("Computing cell areas...")
             self.areas = (self.R**2)*areas
             self.length_x = self.R*self.length_x
             self.length_y = self.R*self.length_y
@@ -565,7 +565,7 @@ class cubed_sphere:
             #print(np.amin(self.length_edy),np.amax(self.length_edy))
             # Generate tangent vectors
             if showonscreen==True:
-                print("Generating latlon tangent vectors...")  
+                print("Generating latlon tangent vectors...")
 
             # Lat-lon tangent unit vectors
             self.elon_edx = sphgeo.tangent_geo_lon(edx.lon)
@@ -587,13 +587,13 @@ class cubed_sphere:
 
             tg_ey_edy.lon = tg_ey_edy.X[:,:,:]*self.elon_edy[:,:,:,0] + tg_ey_edy.Y[:,:,:]*self.elon_edy[:,:,:,1] + tg_ey_edy.Z[:,:,:]*self.elon_edy[:,:,:,2]
             tg_ey_edy.lat = tg_ey_edy.X[:,:,:]*self.elat_edy[:,:,:,0] + tg_ey_edy.Y[:,:,:]*self.elat_edy[:,:,:,1] + tg_ey_edy.Z[:,:,:]*self.elat_edy[:,:,:,2]
-            
+
             self.tg_ex_edx = tg_ex_edx
             self.tg_ey_edx = tg_ey_edx
             self.tg_ex_edy = tg_ex_edy
             self.tg_ey_edy = tg_ey_edy
 
-            # Latlon/Contravariant conversion 
+            # Latlon/Contravariant conversion
             if showonscreen==True:
                 print("Generating latlon/contravariant conversion variables... \n")
 
@@ -624,7 +624,6 @@ class cubed_sphere:
             self.determinant_ll2contra_edy = determinant_ll2contra_edy
             #tg_ex_edx, tg_ey_edx
             #tg_ex_edy, tg_ey_edy
-            
             #self.elon_edx, self.elat_edx
             #self.elon_edy, self.elat_edy
 
@@ -633,20 +632,21 @@ class cubed_sphere:
 
         if showonscreen==True:
             # Print some grid properties
-            print("\nMin  edge length (km)  : ","{:.2e}".format(np.amin(self.length_x[i0:iend+1,j0:jend,:])))
-            print("Max  edge length (km)  : ","{:.2e}".format(np.amax(self.length_x[i0:iend+1,j0:jend,:])))
-            print("Mean edge length (km)  : ","{:.2e}".format(np.mean(self.length_x[i0:iend+1,j0:jend,:])))
+            print("\nMin  edge length (km)  : ","{:.2e}".format(np.amin(self.length_x[i0:iend+1,j0:jend,:])/10**3))
+            print("Max  edge length (km)  : ","{:.2e}".format(np.amax(self.length_x[i0:iend+1,j0:jend,:]/10**3)))
+            print("Mean edge length (km)  : ","{:.2e}".format(np.mean(self.length_x[i0:iend+1,j0:jend,:]/10**3)))
             print("Ratio max/min length   : ","{:.2e}".format(np.amax(self.length_x[i0:iend+1,j0:jend,:])/np.amin(self.length_x[i0:iend+1,j0:jend+1,:])))
-         
-            print("Min  area (km2)        : ","{:.2e}".format(np.amin(self.areas[i0:iend,j0:jend,:])))
-            print("Max  area (km2)        : ","{:.2e}".format(np.amax(self.areas[i0:iend,j0:jend,:])))
-            print("Mean area (km2)        : ","{:.2e}".format(np.mean(self.areas[i0:iend,j0:jend,:])))         
+
+            print("Min  area (km2)        : ","{:.2e}".format(np.amin(self.areas[i0:iend,j0:jend,:])/10**6))
+            print("Max  area (km2)        : ","{:.2e}".format(np.amax(self.areas[i0:iend,j0:jend,:])/10**6))
+            print("Mean area (km2)        : ","{:.2e}".format(np.mean(self.areas[i0:iend,j0:jend,:])/10**6))
             print("Ratio max/min area     : ","{:.2e}".format(np.amax(self.areas[i0:iend,j0:jend,:])/np.amin(self.areas[i0:iend,j0:jend,:])))
-         
+
             print("Min  angle (degrees)   : ","{:.2e}".format(np.amin(self.angles[i0:iend+1,j0:jend+1,:]*rad2deg)))
             print("Max  angle (degrees)   : ","{:.2e}".format(np.amax(self.angles[i0:iend+1,j0:jend+1,:]*rad2deg)))
             print("Mean angle (degrees)   : ","{:.2e}".format(np.mean(self.angles[i0:iend+1,j0:jend+1,:]*rad2deg)))
             print("Ratio max/min angle    : ","{:.2e}".format(np.amax(self.angles[i0:iend+1,j0:jend+1,:])/np.amin(self.angles[i0:iend+1,j0:jend+1,:])))
+
             #m = N
             #ratio = (1+2*np.tan((pi/4))**2 * (1-1/m))**1.5
             #ratio = ratio*np.cos((pi/4))**4*(1-1/m)
@@ -666,7 +666,7 @@ class scalar_field:
 
         # Field position
         self.position = position
-      
+
         # Number of values in a coordinate axis (same for all panels)
         if position == "vertex":
             self.N = grid.N+1
@@ -678,7 +678,7 @@ class scalar_field:
 
         # Field values along a coordinate axis (same for all panels)
         self.f = np.zeros((self.N, self.N, nbfaces))
-      
+
 ####################################################################################
 # Structure for lat-lon grid
 ####################################################################################
@@ -686,35 +686,35 @@ class latlon_grid:
     def __init__(self, Nlat, Nlon):
         self.Nlat, self.Nlon = Nlat, Nlon
 
-        # Create a latlon grid 
+        # Create a latlon grid
         lat = np.linspace(-pio2, pio2, Nlat)
         lon = np.linspace(  -pi,   pi, Nlon)
         self.lat, self.lon = np.meshgrid(lat, lon)
- 
-        # Convert to cartesian coordinates      
-        self.X, self.Y, self.Z = sphgeo.sph2cart(self.lon, self.lat) 
-      
+
+        # Convert to cartesian coordinates
+        self.X, self.Y, self.Z = sphgeo.sph2cart(self.lon, self.lat)
+
         #Lat-lon to cubed sphere indexes
         self.ix, self.jy, self.mask = [], [], []
 
 ####################################################################################
-# Compute execution time of the grid generation for different resolutions 
+# Compute execution time of the grid generation for different resolutions
 ####################################################################################
 def cs_time_test():
     import time
     from matplotlib import pyplot as plt
-   
+
     # Values of N
     Ns = (1,10,100,1000)
-   
+
     # Number we generate the grid to compute the execution time
     Ntest = 10
-   
+
     # Vector that save the execution time for each N
     times = np.zeros(len(Ns))
 
     # Calculate the average execution time using Ntest executions
-    k = 0   
+    k = 0
     for N in Ns:
         for t in range(0,Ntest):
             start_time = time.time()
@@ -725,7 +725,7 @@ def cs_time_test():
         k = k + 1
     print("N and average time (in seconds):")
     k = 0
-   
+
     # Show the value of N and execution time on the screen
     for k in range(0,len(Ns)):
         print(Ns[k], times[k])
@@ -733,7 +733,7 @@ def cs_time_test():
     print('\n')
     for k in range(1,len(Ns)):
         print(Ns[k], times[k]/times[k-1])
-      
+
     # Plot the graph in logscale
     plt.figure()
     plt.loglog(Ns, times,marker='x')
