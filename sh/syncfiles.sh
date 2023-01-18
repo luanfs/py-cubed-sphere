acf01ed50c3db3bef1c96b46594712db92dd8953
luanfs <luanfonseca93@gmail.com>
2023-01-18 12:54:49 -0300

    first commit

---
+++ sh/syncfiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# P. Peixoto - Jul 2012
+# modified by Luan Santos - 2022
+
+date=` date +%F `
+version=` date +%y.%m.%d `
+
+#output="py-dimsplit$version.tar.bz2"
+output="py-cs.tar.bz2"
+bkpdir="py-cs" 
+
+#Edit place to sync relative to system used
+dropdir="/home/luan/Dropbox/doc/code/"$bkpdir
+echo "Sync with Dropbox:"
+rsync -v -t -u $output  "$dropdir/."
+echo "Synchronized with Dropbox"
+echo
+
+#remote server ime.usp.br backup sync
+echo "Sending to ime.usp.br:"
+rsync -t -v -z -a --progress $output luansantos@ime.usp.br:/home/posmap/luansantos/doc/$bkpdir
+echo "Sent to luansantos@ime.usp.br"
+echo
+
+#remote server ybytu backup sync
+echo "Sending to ybytu:"
+ssh -t luansantos@ime.usp.br  "rsync -t -v -z -a --progress doc/$bkpdir/$output luansantos@ybytu:/home/luansantos/doc/"$bkpdir
+echo "Sent to luansantos@ybytu"
+echo
+
+# remove tar file
+rm -rf $output
---
+++ sh/tarfiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Creates a tarball
+
+date=` date +%F `
+version=` date +%y.%m.%d `
+echo "Today: " $date
+
+sourcefiles="src/*.py"
+
+parfiles="par/*.par "
+
+scripts="sh/*.sh "
+
+others="main.py\
+ README.* \
+ backup.sh "
+
+files="$sourcefiles $parfiles $scripts $others"
+
+#output="py-dimsplit$version.tar.bz2"
+output="py-cs.tar.bz2"
+
+tar cjfv $output $files
+
+echo "File " $output " ready!"
+echo
+
+echo "-------------------------------------------"
